97919b5b498cf218b1005724e02ab334c09994ce
MarcBotana <marc.botana@gmail.com>
2025-03-13 14:02:41 +0100

    Initial Commit

---
+++ EntreBicis_Backend/src/main/java/cat/copernic/mbotana/entrebicis_backend/EntrebicisBackendApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.mbotana.entrebicis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrebicisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrebicisBackendApplication.class, args);
+	}
+
+}
---
+++ EntreBicis_Backend/src/test/java/cat/copernic/mbotana/entrebicis_backend/EntrebicisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.copernic.mbotana.entrebicis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrebicisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
