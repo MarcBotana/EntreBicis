ab52964c0a968ed3103e208bf6ee3bfc3c9910cb
Marc Botana <mbotana@alumnat.copernic.cat>
2025-04-10 12:49:42 +0200

    Prueba Pantalla ChangePasswordScree - EmailForm

--- EntreBicis_Backend/src/main/java/cat/copernic/mbotana/entrebicis_backend/security/SecurityConfig.java
+++ EntreBicis_Backend/src/main/java/cat/copernic/mbotana/entrebicis_backend/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package cat.copernic.mbotana.entrebicis_backend.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import cat.copernic.mbotana.entrebicis_backend.entity.SystemParams;
 import cat.copernic.mbotana.entrebicis_backend.entity.User;
@@ -86,6 +91,20 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder)
                 .and()
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", corsConfig);
+
+        return source;
     }
 
     @Bean
