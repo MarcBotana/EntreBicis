033a4120d1e4da7597e54ef9ba2abf9ac476db68
MarcBotana <marc.botana@gmail.com>
2025-04-11 10:26:34 +0200

    Modificació Comentari SecurityConfig

--- EntreBicis_Backend/src/main/java/cat/copernic/mbotana/entrebicis_backend/security/SecurityConfig.java
+++ EntreBicis_Backend/src/main/java/cat/copernic/mbotana/entrebicis_backend/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
+                .csrf(CSRF -> CSRF
                     .disable())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
