5fe91badac73f23625db123b67330bd01a81cc4a
Marc Botana <mbotana@alumnat.copernic.cat>
2025-05-12 15:02:47 +0200

    Docker IP

--- EntreBicis_Frontend/app/src/main/java/cat/copernic/mbotana/entrebicis_frontend/core/common/Constants.kt
+++ EntreBicis_Frontend/app/src/main/java/cat/copernic/mbotana/entrebicis_frontend/core/common/Constants.kt
@@ -1,6 +1,6 @@
 package cat.copernic.mbotana.entrebicis_frontend.core.common
 
 object Constants {
-    //const val BASE_URL = "http://10.0.2.2:8080/api"
-    const val BASE_URL = "http://3.93.177.21:8443/api"
+    const val BASE_URL = "http://10.0.2.2:8080/api"
+    //const val BASE_URL = "http://3.93.177.21:8443/api"
 }
