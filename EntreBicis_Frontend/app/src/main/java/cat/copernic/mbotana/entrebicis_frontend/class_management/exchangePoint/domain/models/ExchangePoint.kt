88821b45b34335f92e779fa737ab91703ac20c6e
Marc Botana <mbotana@alumnat.copernic.cat>
2025-05-28 21:04:40 +0200

    Web Update

--- EntreBicis_Frontend/app/src/main/java/cat/copernic/mbotana/entrebicis_frontend/class_management/exchangePoint/domain/models/ExchangePoint.kt
+++
@@ -1,7 +0,0 @@
-package cat.copernic.mbotana.entrebicis_frontend.class_management.exchangePoint.domain.models
-
-data class ExchangePoint(
-    var id: Long? = null,
-    var name: String,
-    var address: String
-)
